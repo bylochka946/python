71b51f17dc5b4d9bc1fb6bda9d90dd7531ff4e49
bylochka946 <sofiadobranskaa3@gmail.com>
2025-01-20 23:20:57 +0300

    Update task_2.py

--- Лабораторная 2/task_2.py
+++ Лабораторная 2/task_2.py
@@ -1,4 +1,3 @@
-
 from typing import Optional
 
 BOOKS_DATABASE = [
@@ -17,6 +16,13 @@ BOOKS_DATABASE = [
 
 class Book:
     def __init__(self, id_, name, pages):
+        """
+        Создание и подготовка к работе экземпляра класса "Book"
+
+        :id_: Идентификатор книги
+        :name: Название книги
+        :pages: Количество страниц в книге
+        """
         self.id = id_
         self.name = name
         self.pages = pages
@@ -24,15 +30,30 @@ class Book:
 
 class Library:
     def __init__(self, books=None):
+        """
+        Создание и подготовка к работе экземпляра класса "Library"
+
+        :books: Список книг
+        """
         self.books = books
 
     def get_next_book_id(self):
+        """
+        Метод, возвращающий идентификатор для добавления новой книги в библиотеку
+
+        return: Идентификатор последней книги увеличенный на 1
+        """
         if self.books:
             return len(self.books) + 1
         else:
             return 1
 
     def get_index_by_book_id(self, id_):
+        """
+        Метод, возвращающий индекс книги в списке, который хранится в атрибуте экземпляра класса
+
+        return: Индекс из списка (если книга существует) | Ошибка (если книги нет)
+        """
         for index, book in enumerate(self.books):
             if book.id == id_:
                 return index
